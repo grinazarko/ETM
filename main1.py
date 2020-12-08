5a6594798090cf1bd587b92ce988f27a312b54a1
Grigorii Nazarko <grigoriinazarko@Olgas-MBP.home>
2020-12-08 13:34:49 +0200

    foo

--- main1.py
+++ main1.py
@@ -2,6 +2,16 @@ import re
 import numpy as np
 import pandas as pd
 import pickle
+import pip
+
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+install("top2vec")
+install("top2vec[sentence_encoders]")
 
 dataset = pd.read_csv('/valohai/inputs/data/lastFiveYears.csv')
 dataset = dataset[dataset['language'] == "en"]
