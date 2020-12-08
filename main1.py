867dc0822a84718313f504d5ce1d5e7ac12015ef
Grigorii Nazarko <grigoriinazarko@Olgas-MBP.home>
2020-12-08 12:11:30 +0200

    foo

---
+++ main1.py
@@ -0,0 +1,14 @@
+import re
+import numpy as np
+import pandas as pd
+
+dataset = pd.read_csv('/valohai/inputs/data/lastFiveYears.csv')
+dataset = dataset[dataset['language'] == "en"]
+print(dataset.shape)
+
+from top2vec import Top2Vec
+
+model = Top2Vec(list(dataset["txtBody_Clean"])[:100], embedding_model='universal-sentence-encoder')
+
+with open('/valohai/outputs/file', 'wb') as f:
+    pickle.dump(model, f)
